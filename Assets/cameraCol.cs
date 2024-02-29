c28d91a6187f02195679777a1e8ff924c6e3879a
joshberger5 <bergjo7@furman.edu>
2024-02-29 16:01:24 -0500

    finally fixed camera collision, range updates

--- Assets/cameraCol.cs
+++ Assets/cameraCol.cs
@@ -5,15 +5,30 @@ using UnityEngine;
 public class cameraCol : MonoBehaviour
 {
     [SerializeField]
-    List<string> tags;
+    List<string> tags;  // the tags of objects the camera should collide with
+
+    Vector3 lastPosition; // holds the previous position of the camera
+
+    private void Start() {
+        lastPosition = transform.position; // initializes the previous position
+    }
+
+    private void Update() {
+        if (Vector3.Distance(transform.position, lastPosition) > 0.1f) 
+        {
+            lastPosition = transform.position; // if the camera has moved more than 0.1 units, update the previous position
+        }
+    }
 
     private void OnCollisionEnter(Collision collision)
     {   
         foreach (string tag in tags)
         {
             if (collision.gameObject.tag == tag)
-            {
-                transform.parent.position = new Vector3(transform.parent.position.x - transform.forward.x, transform.parent.position.y, transform.parent.position.z - transform.forward.z);
+            {   
+                Vector3 difference = transform.position - lastPosition; // when the camera collides with something, move it back to the previous position
+                difference.y = 0;
+                transform.parent.position -= difference;
             }
         }
     }
