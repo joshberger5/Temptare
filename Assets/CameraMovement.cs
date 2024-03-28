6d58500804c66c4e42807e38efa2cd5a58ab96ce
joshberger5 <bergjo7@furman.edu>
2024-03-28 14:39:40 -0400

    preliminary training course scene

--- Assets/CameraMovement.cs
+++ Assets/CameraMovement.cs
@@ -6,6 +6,10 @@ public class CameraMovement : MonoBehaviour
 {
     private bool move = false; // boolean holds whether the camera should move
 
+    private Vector3 moveDirection = Vector3.forward; // vector3 holds the direction the camera should move
+
+    private int moveAmount = 1; // int holds the amount the camera should move
+
     public void Move() 
     { 
         // sets move to true
@@ -17,13 +21,28 @@ public class CameraMovement : MonoBehaviour
         // if move is true, move the camera forward
         if (move)
         {
+            transform.parent.Translate(moveDirection * moveAmount * Time.deltaTime);
+            /*
             if (transform.parent.position.x == 0 && transform.parent.position.z < 10) 
             {
-                transform.parent.Translate(Vector3.forward * Time.deltaTime);
+                transform.parent.Translate(moveDirection * Time.deltaTime);
             }
             else {
                 transform.parent.Translate(Vector3.right * Time.deltaTime);
             }
+            */
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "CameraCollider") // change the direction of the camera's motion
+        {
+            moveDirection = other.transform.forward;
+        }
+        else if (other.gameObject.tag == "CameraStopper") // stop the camera
+        {
+            moveAmount = 0;
         }
     }
 }
--- Assets/StartMovement.cs
+++ Assets/StartMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class OnCollisionDestroy : MonoBehaviour
+public class StartMovement : MonoBehaviour
 {
     [SerializeField]
     GameObject camera;
